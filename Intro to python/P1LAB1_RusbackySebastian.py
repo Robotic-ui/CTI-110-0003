8f75cf3c9a7a8dcae215eca42fef271909ec18c2
Robotic <84217158+Robotic-ui@users.noreply.github.com>
2024-02-15 10:24:48 -0500

    Add files via upload

---
+++ Intro to python/P1LAB1_RusbackySebastian.py
@@ -0,0 +1,8 @@
+# Sebastian Rusbacky
+# 2/13/2024
+# P1LAB1
+# gets user input thru a var then displays that input to the screen with concatenation
+
+name = input("Enter your name: ")
+print("Hey", name)
+print("Welcome to ZyBooks!")
