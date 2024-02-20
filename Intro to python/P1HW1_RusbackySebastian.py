66828bfd0de301facddafe51ddcb6b90305e841e
Robotic <84217158+Robotic-ui@users.noreply.github.com>
2024-02-20 11:10:59 -0500

    Update P1HW1_RusbackySebastian.py

--- Intro to python/P1HW1_RusbackySebastian.py
+++ Intro to python/P1HW1_RusbackySebastian.py
@@ -1,6 +1,10 @@
+# Sebastian Rusbacky
+# 2/18/2024
+# P1HW1
+# Does basic math, collects user input then dispays the result
+
 first_num = int(input("Enter an integer: "))
 print("You entered:", first_num)
-
 print(first_num, "Squared is:", first_num ** 2)
 print(first_num, "Cubed is:", first_num ** 3)
 
