5d2a9c0b68a530039393d54c052b3e277e615b53
Robotic <84217158+Robotic-ui@users.noreply.github.com>
2024-02-27 16:07:05 +0000

    New file P2LAB1

---
+++ Intro to python/P2LAB1_RusbackySebastian.py
@@ -0,0 +1,6 @@
+# Sebastian Rusbacky
+# 2/27/2024
+# P2LAB1
+# a program with a car's gas mileage (miles/gallon) and the cost of gas (dollars/gallon) 
+# as floating-point input, and output the gas cost for 20 miles, 75 miles, and 500 miles.
+
