7b6947e4460e0b530e91b7df4cc060b9718e1e7e
Robotic <84217158+Robotic-ui@users.noreply.github.com>
2024-02-29 16:09:56 +0000

    New file intro to Python > P2HW1_RusbackySebastian

---
+++ Intro to python/P2HW2_RusbackySebastian.py
@@ -0,0 +1,22 @@
+# Sebastian Rusbacky
+# 2/29/2024
+# P2HW2
+# Accessing 
+
+mod1 = input("What is your Grade for Module 1? "))
+mod2 = input("What is your Grade for Module 2? ")
+mod3 = input("What is your Grade for Module 3? ")
+mod4 = input("What is your Grade for Module 4? ")
+mod5 = input("What is your Grade for Module 5? ")
+mod6 = input("What is your Grade for Module 6? ")
+
+moduleGrades =
+[
+    
+]
+
+print("--------- Results ----------")
+print("Lowest Grade: ")
+print("Highest Grade: ")
+print("Sum of Grades: ")
+print("Average: ")
