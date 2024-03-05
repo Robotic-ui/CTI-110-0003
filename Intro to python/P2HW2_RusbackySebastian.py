dd525942ab1c103b533c1a91a80a8d5fb4f191f6
Robotic <84217158+Robotic-ui@users.noreply.github.com>
2024-03-05 15:59:46 +0000

    Updated P2HW2_RusbackySebastian.py

--- Intro to python/P2HW2_RusbackySebastian.py
+++ Intro to python/P2HW2_RusbackySebastian.py
@@ -1,22 +1,19 @@
 # Sebastian Rusbacky
 # 2/29/2024
 # P2HW2
-# Accessing 
+# Accessing a user entered list then dsplaying lowestGrade, highestGrade, the sum of all the grades and average 
 
-mod1 = input("What is your Grade for Module 1? "))
-mod2 = input("What is your Grade for Module 2? ")
-mod3 = input("What is your Grade for Module 3? ")
-mod4 = input("What is your Grade for Module 4? ")
-mod5 = input("What is your Grade for Module 5? ")
-mod6 = input("What is your Grade for Module 6? ")
+mod1_Grade = float(input("What is your Grade for Module 1? "))
+mod2_Grade = float(input("What is your Grade for Module 2? "))
+mod3_Grade = float(input("What is your Grade for Module 3? "))
+mod4_Grade = float(input("What is your Grade for Module 4? "))
+mod5_grade = float(input("What is your Grade for Module 5? "))
+mod6_Grade = float(input("What is your Grade for Module 6? "))
 
-moduleGrades =
-[
-    
-]
+grades = [mod1_Grade, mod2_Grade, mod3_Grade, mod4_Grade, mod5_grade, mod6_Grade]
 
 print("--------- Results ----------")
-print("Lowest Grade: ")
-print("Highest Grade: ")
-print("Sum of Grades: ")
-print("Average: ")
+print(f"Lowest Grade: {lowest_grade}")
+print(f"Highest Grade: {highest_grade}")
+print(f"Sum of Grades: {grades_sum}")
+print(f"Average: {average}")
