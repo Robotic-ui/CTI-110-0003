a717eeb1622de2f97454565e5e2305225dbf8692
Robotic <84217158+Robotic-ui@users.noreply.github.com>
2024-03-06 15:52:22 +0000

    Updated P2HW2

--- Intro to python/P2HW2_RusbackySebastian.py
+++ Intro to python/P2HW2_RusbackySebastian.py
@@ -11,8 +11,19 @@ mod5_grade = float(input("What is your Grade for Module 5? "))
 mod6_Grade = float(input("What is your Grade for Module 6? "))
 
 grades = [mod1_Grade, mod2_Grade, mod3_Grade, mod4_Grade, mod5_grade, mod6_Grade]
+lowestGrade = min(grades)
+highestGrade = max(grades)
+gradesSum = sum(grades)
+
+# first slower method to find the average
+average = mod1_Grade + mod2_Grade + mod3_Grade + mod4_Grade + mod5_grade + mod6_Grade / 2
+
+# faster way
+average = sum(grades) /len(grades)
+print(f"Second average method" round(average,3))
 
 print("--------- Results ----------")
-print(f"Lowest Grade: {}")
-print(f"Highest Grade: {}")
-print(f"Sum of Grades: {}")
+print(f"Lowest Grade: {lowestGrade}")
+print(f"Highest Grade: {highestGrade}")
+print(f"Sum of Grades: {gradesSum}")
+print(f"Average: {average}")
