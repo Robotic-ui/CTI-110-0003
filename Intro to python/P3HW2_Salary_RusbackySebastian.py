405ac639abdd218e41d8fa6cf7a66e79c0a88a2f
Sebastian Rusbacky <84217158+Robotic-ui@users.noreply.github.com>
2024-03-21 15:04:32 +0000

    Updated P3HW2

--- Intro to python/P3HW2_Salary_RusbackySebastian.py
+++ Intro to python/P3HW2_Salary_RusbackySebastian.py
@@ -1,6 +1,23 @@
-employeeName = str(input("Enter employee's name: "))
-hoursWorked = int(input("Enter hours name: "))
+employeeName = input("Enter employee's name: ")
+hoursWorked = int(input("Enter hours worked: "))
 payRate = float(input("Enter employee's pay rate: "))
 
 print('--------------------')
-print(f"Employee name {employeeName} :<10")
+print(f"{'Employee name:':<10} {employeeName}")
+print()
+
+if hoursWorked < 40:
+    regHours = hoursWorked
+    overtimeHours = 0
+    print(" No Overtime! \n")
+else:  # hoursWorked >= 40
+    regHours = 40
+    overtimeHours = hoursWorked - 40
+
+regHourPay = regHours * payRate
+overtimePay = overtimeHours * (1.5 * payRate)
+grossPay = regHourPay + overtimePay
+
+print(f"{'Hours Worked':^10}{'Pay Rate':^20}{'OverTime':<15}{'OverTime Pay':<15}{'RegHour Pay':^15}{'Gross Pay':^15}")
+print('-------------------------------------------------------------------------------------------------------')
+print(f"{hoursWorked:^10}{payRate:^20}{overtimeHours:^15}${overtimePay:<15.2f}${regHourPay:<15.2f}${grossPay:<15.2f}")
