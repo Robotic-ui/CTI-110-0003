161eb5c8a885d95c4de251ad19a4d72cabf7461e
Sebastian Rusbacky <84217158+Robotic-ui@users.noreply.github.com>
2024-03-21 14:24:57 +0000

    New File P3HW2

---
+++ Intro to python/P3HW2_Salary_RusbackySebastian.py
@@ -0,0 +1,6 @@
+employeeName = str(input("Enter employee's name: "))
+hoursWorked = int(input("Enter hours name: "))
+payRate = float(input("Enter employee's pay rate: "))
+
+print('--------------------')
+print(f"Employee name {employeeName} :<10")
